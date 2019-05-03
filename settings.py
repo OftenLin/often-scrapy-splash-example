7fcd221c42f64b6f9ca6871ba2c3227e14d80435
Often <ab-jack@hotmail.com.tw>
2019-05-04 01:28:03 +0800

    Set up SplashDeduplicateArgsMiddleware

--- settings.py
+++ settings.py
@@ -47,9 +47,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'fsc_finanical_dictionary.middlewares.FscFinanicalDictionarySpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
